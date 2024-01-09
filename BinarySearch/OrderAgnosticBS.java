45c082b16ff2084e28635fa74be14f9c8490c564
ItsProgrammerVinay <119567736+ItsProgrammerVinay@users.noreply.github.com>
2024-01-09 17:20:17 +0000

    Binary Search algorithm

--- BinarySearch/OrderAgnosticBS.java
+++ BinarySearch/OrderAgnosticBS.java
@@ -2,8 +2,9 @@ package BinarySearch;
 
 public class OrderAgnosticBS {
   public static void main(String[] args) {
-    int arr[] = { 40, 20, 10, 5, 2, 6 };
-    System.out.println(orderAgnosticBs(arr, 10));
+    // int arr[] = { 40, 20, 10, 5, 2, 6 };
+    int arr[]={2,4,5,6,7,8,9};
+    System.out.println(orderAgnosticBs(arr, 9));
   }
 
   static int orderAgnosticBs(int arr[], int tar) {
--- BinarySearch/Searching.java
+++ BinarySearch/Searching.java
@@ -16,7 +16,7 @@ public class Searching {
   static int binarySearch(int arr[], int target) {
 
     int start = 0;
-    int end = arr.length ;
+    int end = arr.length-1 ;
     
     while (start <= end) {
       //find the middle element -->
