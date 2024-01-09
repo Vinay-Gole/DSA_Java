bd2ba2b929e0ec74f1f5dc29d4856f24fc017e6d
ItsProgrammerVinay <119567736+ItsProgrammerVinay@users.noreply.github.com>
2024-01-09 16:50:33 +0000

    Binary Search Algorithm

---
+++ BinarySearch/Searching.java
@@ -0,0 +1,36 @@
+package BinarySearch;
+
+
+import java.util.Arrays;
+
+public class Searching {
+  public static void main(String[] args) {
+
+    int arr[]={2,4,5,6,7,8,9};
+    Arrays.sort(arr);
+     System.out.println(binarySearch(arr, 7));
+
+
+  }
+  //----------------------------------------------------------------------------
+  static int binarySearch(int arr[], int target) {
+
+    int start = 0;
+    int end = arr.length ;
+    
+    while (start <= end) {
+      //find the middle element -->
+      int mid = start + (end - start) / 2;
+      
+      if (target <arr[mid] ) {
+        end = mid - 1;
+      } else if (target > arr[mid]) {
+        start = mid + 1;
+      } else {
+        return mid;
+      }
+    }
+    return-1;
+  }
+  
+}
