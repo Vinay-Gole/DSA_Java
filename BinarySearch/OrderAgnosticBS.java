2a54f4cafb335566483c568b736d1d50f9b44f0b
ItsProgrammerVinay <119567736+ItsProgrammerVinay@users.noreply.github.com>
2024-01-09 17:15:37 +0000

    Order Agnostic Binary Search

---
+++ BinarySearch/OrderAgnosticBS.java
@@ -0,0 +1,37 @@
+package BinarySearch;
+
+public class OrderAgnosticBS {
+  public static void main(String[] args) {
+    int arr[] = { 40, 20, 10, 5, 2, 6 };
+    System.out.println(orderAgnosticBs(arr, 10));
+  }
+
+  static int orderAgnosticBs(int arr[], int tar) {
+    
+    int start = 0;
+    int end = arr.length-1;
+    boolean isAsc = arr[start] < arr[end];
+
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+
+      if (arr[mid] == tar) {
+        return mid;
+      }
+      if (isAsc) {
+        if (tar < arr[mid]) {
+          end = mid - 1;
+        } else {
+          start = mid + 1;
+        }
+      } else {
+        if (tar > arr[mid]) {
+          end = mid - 1;
+        } else {
+          start = mid + 1;
+        }
+      }
+    }
+    return -1;
+  }
+}
