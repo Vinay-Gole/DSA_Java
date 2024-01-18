8b246182fe30266be9ba5fb7734d284e17fb4196
ItsProgrammerVinay <119567736+ItsProgrammerVinay@users.noreply.github.com>
2024-01-18 16:37:53 +0000

    bubble sort algorithm

---
+++ SortingAlgorithms/BubbleSort.java
@@ -0,0 +1,26 @@
+package SortingAlgorithms;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+  public static void main(String[] args) {
+    int nums[] = { 8, 5, 3, 1, 3, 4 };
+    bubbleSort(nums);
+    System.out.println(Arrays.toString(nums));
+
+  }
+  
+  static void bubbleSort(int arr[]) {
+    int n = arr.length;
+    for(int i=0;i<n;i++){
+      for (int j = 1; j < n - i - 1; j++) {
+        if (arr[j] < arr[j - 1]) {
+          int temp = arr[j];
+          arr[j] = arr[j - 1];
+          arr[j - 1] = temp;
+        }
+      }
+    }
+
+  }
+}
